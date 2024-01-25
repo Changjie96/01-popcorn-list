1457586910e85701d8e961a1486c97e86f335ea0
ChangJie Tan <tan7cj@gmail.com>
2024-01-25 13:47:46 +0800

    website done

---
+++ public/helper/useLocalStorageState.js
@@ -0,0 +1,37 @@
+'use client'
+// import { useState, useEffect } from "react";
+
+// export function useLocalStorageState(initialState, key) {
+//   const [value, setValue] = useState(function () {
+//     const storedValue = localStorage.getItem(key);
+//     return storedValue ? JSON.parse(storedValue) : initialState;
+//   });
+
+//   useEffect(function() {
+//     localStorage.setItem(key, JSON.stringify(value))
+//   }, [value, key]);
+
+//   return [value, setValue];
+// }
+import { useState, useEffect } from "react";
+
+export function useLocalStorageState(initialState, key) {
+  const isLocalStorageAvailable = typeof localStorage !== "undefined";
+
+  const [value, setValue] = useState(function () {
+    if (isLocalStorageAvailable) {
+      const storedValue = localStorage.getItem(key);
+      return storedValue ? JSON.parse(storedValue) : initialState;
+    } else {
+      return initialState;
+    }
+  });
+
+  useEffect(function() {
+    if (isLocalStorageAvailable) {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
+  }, [value, key, isLocalStorageAvailable]);
+
+  return [value, setValue];
+}
